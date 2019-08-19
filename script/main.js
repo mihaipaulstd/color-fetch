7b7cc5b569daf0fe49a71d4383390f1aa7d8fc42
mihaipaulstd <mihaipaulstd@gmail.com>
2019-08-19 15:35:28 +0300

    small structuring improvements

--- script/main.js
+++ script/main.js
@@ -6,7 +6,18 @@ var global = {
     },
     variables: {
         colors: new Array(),
-        initialColor: new String()
+        darkColor: new String(),
+        lightColor: new String()
+    },
+    functions: {
+        setDarkColor: function() {
+            let color = global.variables.colors[Math.floor(Math.random() * global.variables.colors.length)];
+            color.luminance <= 50 ? global.variables.darkColor = color : this.setDarkColor();
+        },
+        setLightColor: function() {
+            let color = global.variables.colors[Math.floor(Math.random() * global.variables.colors.length)];
+            color.luminance >= 100 ? global.variables.lightColor = color : this.setLightColor();
+        }
     }
 
 };
@@ -14,8 +25,7 @@ var global = {
 window.addEventListener('load', init);
 
 function init() {
-    setup();
-    console.log();
+    run();
     
     
 }
--- script/run.js
+++ script/run.js
@@ -1,37 +1,31 @@
-function setup() {
+function run() {
     fetch('http://color-names.herokuapp.com/v1/')
         .then(res => res.json())
         .then(obj => obj.colors)
         .then(colors => {
-            function getColor() {
-                let color = colors[Math.floor(Math.random() * colors.length)];
-                console.log(color);
-
-                return color.luminance < 50 ? color : getColor();
-            }
-            global.variables.initialColor = getColor();
+            colors.forEach(color => {
+                global.variables.colors.push(color);
+            });
             screen();
-            return colors;
-        })
-        .then(colors => colors.forEach(color => {
-            global.variables.colors.push(color);
-        }));
+        });
         
         
     function screen() {
         setColors();
         setDelay();
         function setColors() {
+            global.functions.setDarkColor();
+            global.functions.setLightColor();
             const screen = global.elements.screen;
             const input = global.elements.input;
             const time = global.elements.time;
+            const darkColor = global.variables.darkColor;
+            const lightColor = global.variables.lightColor;
             
-            screen.style.backgroundColor = global.variables.initialColor.hex;
+            screen.style.backgroundColor = darkColor.hex;
+            input.style.color = lightColor.hex;
             input.style.backgroundColor = 'transparent';
-            input.style.color = '#fff';
-            input.style.textShadow = '2px 2px 1px #222'
             time.style.backgroundColor = 'transparent';
-            
             
             
     
@@ -39,10 +33,13 @@ function setup() {
         function setDelay(){
             setTimeout(() => {
                 global.elements.input.style.opacity = 1;
-            }, 1000)
+            }, 2000)
             setTimeout(() => {
                 global.elements.time.style.opacity = 1;
-            }, 1500)
+            }, 2500)
+            
         }
+        
     }
+
 }
