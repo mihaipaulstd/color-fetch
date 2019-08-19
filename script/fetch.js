66329cb7da1edd52df9be9177c6732536312297e
mihaipaulstd <mihaipaulstd@gmail.com>
2019-08-19 23:48:09 +0300

    fetching colors

--- script/fetch.js
+++ script/fetch.js
@@ -1,16 +1,13 @@
-function run() {
+async function fetchColors() {
     console.log('running');
     
-    fetch('http://color-names.herokuapp.com/v1/')
+    await fetch('http://color-names.herokuapp.com/v1/')
         .then(res => res.json())
         .then(obj => obj.colors)
         .then(colors => {
             colors.forEach(color => {
-                global.variables.colors.push(color);
+                global.colors.push(color);
             });
-            
         });
-        
-        
-
+    return this;
 }
--- script/main.js
+++ script/main.js
@@ -1,28 +1,24 @@
 var global = {
-    elements: {
-        screen: document.getElementById('screen'),
-        input: document.getElementById('input'),
-        colorSpan: document.getElementById('color-span')
-    },
-    variables: {
-        gameOver: true,
-        inputValue: new String(),
-        targetColor: new String(),
-        colors: new Array(),
-        darkColor: new String(),
-        lightColor: new String(),
-        colorX: new Number(),
-        colorY: new Number()
-    },
-    functions: {
-            
-    }
+    screen: document.getElementById('screen'),
+    input: document.getElementById('input'),
+    colorSpan: document.getElementById('color-span'),
 
+    gameOver: true,
+    inputValue: new String(),
+    targetColor: new String(),
+    colors: new Array(),
+    darkColor: new String(),
+    lightColor: new String(),
+    colorX: new Number(),
+    colorY: new Number(),
 };
 
 window.addEventListener('load', init);
 
 function init() {
-    run();
-    
+    fetchColors()
+        .then(() => {
+            setup();
+
+        })
 }
