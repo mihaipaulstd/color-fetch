5c1403ec96cbc472d558ba9b9ce425662d0eac8b
mihaipaulstd <mihaipaulstd@gmail.com>
2019-08-21 13:37:17 +0300

    font family changed

--- script/listen/color.js
+++ script/listen/color.js
@@ -1,8 +1,8 @@
-function colorListen() {  
-  if(global.inputValue === global.target.targetColor.name) {
+function colorListen() {
+  if (global.inputValue === global.target.targetColor.name) {
     global.screen.style.backgroundColor = global.target.targetColor.hex;
-    reset(global.input, 1000);
+    reset(global.input, 2000, false, undefined, 0);
     generateTarget();
-    reset(global.colorSpan, 1000, false, global.target.targetColor.name, 0);
+    reset(global.colorSpan, 1500, false, global.target.targetColor.name, 1500);
   }
 }
